8d69b38ffe212e93ff1bd92cbff467adff0ee7e4
Ilian Häggmark <ilianh@kth.se>
2022-01-17 14:24:48 +0900

    Added collapse code

--- externaljs.js
+++ externaljs.js
@@ -4,3 +4,19 @@ function changeContrastMode() {
    var element = document.body;
    element.classList.toggle("contrast-mode");
 }
+
+//CODE TO SHOW/HIDE (COLLAPSE) CONTENT, E.G. PAPERS/PRESENTATIONS 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
