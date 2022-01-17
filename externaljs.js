859425aef2b7e5da78fff3a07ffc9d231bd6eff2
Ilian Häggmark <ilianh@kth.se>
2022-01-17 11:36:15 +0900

    test

--- externaljs.js
+++ externaljs.js
@@ -1,19 +1,3 @@
-
-//CODE TO SHOW/HIDE CONTENT EPAPERS/PRESENTATIONS 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
   
 // CODE TO CHANGE CONTRAST MODE
 function changeContrastMode() {
